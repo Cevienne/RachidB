66f843d1051a071d42b04c20dc1ae461ecb143d8
Celine <celinevbenachour@gmail.com>
2024-10-06 19:41:40 +0200

    update files

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,69 @@
 <?php
 
-    $to = "spn8@spondonit.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+// Composer Autoloading einbinden
+require 'vendor/autoload.php';
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+// Verwende den Namespace von PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $subject = "You have a message from your Bitmap Photography.";
+// Erstelle eine neue Instanz von PHPMailer
+$mail = new PHPMailer(true);
 
+try {
+    // SMTP-Einstellungen
+    $mail->isSMTP(); // Setze Mailer auf SMTP
+    $mail->Host = 'smtp.ionos.de'; // Setze den SMTP-Server
+    $mail->SMTPAuth = true; // Aktiviere SMTP-Authentifizierung
+    $mail->Username = 'contact@rachidb.de'; // Deine E-Mail-Adresse (für SMTP-Authentifizierung)
+    $mail->Password = 'dein_passwort'; // Dein Passwort (für SMTP-Authentifizierung)
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Aktiviere TLS-Verschlüsselung
+    $mail->Port = 587; // TCP-Port für TLS
+
+    // Benutzerinformationen aus dem Kontaktformular
+    $from = $_REQUEST['email']; // E-Mail des Benutzers
+    $name = $_REQUEST['name']; // Name des Benutzers
+    $subject = $_REQUEST['subject']; // Betreff des Benutzers
+    $cmessage = $_REQUEST['message']; // Nachricht des Benutzers
+
+    // Setze Absender und Empfänger
+    $mail->setFrom('contact@rachidb.de', 'Dein Name'); // Setze den Absender (deine eigene E-Mail)
+    $mail->addAddress('contact@rachidb.de'); // Empfänger-E-Mail (deine E-Mail-Adresse)
+
+    // Optional: Antwortadresse
+    $mail->addReplyTo($from, $name); // Antwortadresse
+
+    // Betreff und Inhalt der E-Mail
+    $mail->isHTML(true); // Setze das E-Mail-Format auf HTML
+    $mail->Subject = "Neue Nachricht von {$name} - {$subject}"; // Betreff
+
+    // Nachricht aufbauen
     $logo = 'img/logo.png';
     $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+    $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+    $body .= "<table style='width: 100%;'>";
+    $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+    $body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+    $body .= "</td></tr></thead><tbody><tr>";
+    $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+    $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+    $body .= "</tr>";
+    $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+    $body .= "<tr><td></td></tr>";
+    $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+    $body .= "</tbody></table>";
+    $body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
+    $mail->Body = $body; // Setze den HTML-Inhalt der E-Mail
 
+    // E-Mail senden
+    if ($mail->send()) {
+        echo "E-Mail wurde erfolgreich gesendet.";
+    } else {
+        echo "E-Mail konnte nicht gesendet werden. Fehler: {$mail->ErrorInfo}";
+    }
+} catch (Exception $e) {
+    echo "E-Mail konnte nicht gesendet werden. Fehler: {$mail->ErrorInfo}";
+}
 ?>
--- js/contact.js
+++ js/contact.js
@@ -1,85 +1,79 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
     (function($) {
         "use strict";
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+        jQuery.validator.addMethod('answercheck', function(value, element) {
+            return this.optional(element) || /^\bcat\b$/.test(value);
+        }, "type the correct answer -_-");
 
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
+        // validate contactForm form
+        $(function() {
+            $('#contactForm').validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
                     },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
+                    subject: {
+                        required: true,
+                        minlength: 4
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message: {
+                        required: true,
+                        minlength: 20
                     }
-                })
-            }
-        })
-    })
-        
- })(jQuery)
-})
+                },
+                messages: {
+                    name: {
+                        required: "Bitte füge hier noch deinen Namen ein.",
+                        minlength: "Dein Name muss mindestens aus zwei Buchstaben bestehen"
+                    },
+                    subject: {
+                        required: "Um welche Veranstaltung handelt es sich?",
+                        minlength: "Die Veranstaltung muss mindestens vier Buchstaben haben."
+                    },
+                    email: {
+                        required: "Keine E-Mail? Ich benötige eine, um mich bei dir zu melden."
+                    },
+                    message: {
+                        required: "Bitte beschreibe deine Art von Veranstaltung und ergänze Datum, Zeitraum, etc.",
+                        minlength: "Dies sind zu wenig Informationen, bitte füge weitere hinzu."
+                    }
+                },
+                submitHandler: function(form) {
+                    console.log("Submit handler reached"); // Debugging
+                    $(form).ajaxSubmit({
+                        type: "POST",
+                        data: $(form).serialize(),
+                        url: "contact_process.php",
+                        success: function(response) {
+                            console.log("Form submitted successfully"); // Debugging
+                            console.log("Server Response: " + response); // Protokollierung der Serverantwort
+                            $('#contactForm :input').attr('disabled', 'disabled');
+                            $('#contactForm').fadeTo("slow", 1, function() {
+                                $(this).find(':input').attr('disabled', 'disabled');
+                                $(this).find('label').css('cursor', 'default');
+                                $('#success').fadeIn();
+                                $('.modal').modal('hide');
+                                $('#success').modal('show');
+                            });
+                        },
+                        error: function(jqXHR, textStatus, errorThrown) {
+                            console.log("AJAX-Error: " + textStatus + ", " + errorThrown); // Fehlerprotokollierung
+                            console.log("Response Text: " + jqXHR.responseText); // Protokollierung des vollständigen Antworttexts
+                            $('#contactForm').fadeTo("slow", 1, function() {
+                                $('#error').fadeIn();
+                                $('.modal').modal('hide');
+                                $('#error').modal('show');
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    })(jQuery);
+});
